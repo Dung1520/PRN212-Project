e73be71a622ac05fc3c57be00b155b67e1401218
Nguyễn Hoàng <hoangcdt10@gmail.com>
2026-03-14 10:30:30 +0700

    add nuget package

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eb4cd007676e9194642da85329bbde55f70e1ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fec4ec63eb5ec0ffaa7379467adc43f0bb7a2359")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
