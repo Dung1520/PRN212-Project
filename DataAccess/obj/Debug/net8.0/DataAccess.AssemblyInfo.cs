1e464c0bd4a522ff6fe0bb00f5f8c89fcb228795
Nguyễn Hoàng <hoangcdt10@gmail.com>
2026-03-19 11:34:24 +0700

    add dbcontext

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4a737225d3af2725deb54b44d1d86ece62850fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c79e3da9e86d5f316d0a76db999ed7c47cae9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
